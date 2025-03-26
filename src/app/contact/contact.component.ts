f8e8bde4fed7433a9095a9967fd2368b535c9b99
Marco Oppel <marOpp02@outlook.com>
2025-03-26 16:34:35 +0200

    began with email form functionality

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { AbstractControl, FormControl, FormGroup, NgControl, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './contact.component.html',
   styleUrl: './contact.component.scss'
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit{
+  contact: FormGroup = new FormGroup({
+    name: new FormControl('', [
+      Validators.required
+    ]),
+    email: new FormControl('', [
+      Validators.required,
+      Validators.email
+    ]),
+    message: new FormControl('', [
+      Validators.required
+    ])
+  });
 
+  ngOnInit()
+  {
+    console.log(this.contact);
+  }
+
+  onSubmit()
+  {
+    console.log(this.contact.controls);
+    if (this.contact.invalid)
+      {
+        console.log("INVALID FORM");
+        return;
+      }
+      console.log(this.contact);
+  }
 }
